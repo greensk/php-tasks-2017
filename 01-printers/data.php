e3c1a8b0268bb0756bc0b2bc438fe3cc351be55d
Grigory Kosheev <g2@green.novosibirsk.ru>
2017-10-31 09:27:19 +0700

    add data

---
+++ 01-printers/data.php
@@ -0,0 +1,375 @@
+<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 13444336,
+    'name' => 'L805',
+    'kind' => 'принтер',
+    'description' => 'принтер, A4, печать  пьезоэлектрическая струйная, 6-цветная, 37 стр/мин ч/б, 38 стр/мин цветн., USB, Wi-Fi, печать фото, печать на CD/DVD',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '18699',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13444336.jpeg',
+      'width' => 701,
+      'height' => 489,
+    ),
+    'link' => 'https://market.yandex.ru/product/13444336?hid=138608&pp=1001',
+    'vendorId' => 153058,
+    'vendor' => 'Epson',
+    'isGroup' => false,
+    'reviewsCount' => 10,
+    'rating' => 4.0,
+    'offersCount' => 82,
+    'gradeCount' => 12,
+    'articlesCount' => 2,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 12659940,
+    'name' => 'L222',
+    'kind' => 'МФУ',
+    'description' => 'МФУ, A4, печать  пьезоэлектрическая струйная, 4-цветная, 27 стр/мин ч/б, 15 стр/мин цветн., USB, печать фото',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '13200',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12659940.jpeg',
+      'width' => 701,
+      'height' => 347,
+    ),
+    'link' => 'https://market.yandex.ru/product/12659940?hid=138608&pp=1001',
+    'vendorId' => 153058,
+    'vendor' => 'Epson',
+    'isGroup' => false,
+    'reviewsCount' => 10,
+    'rating' => 3.5,
+    'offersCount' => 87,
+    'gradeCount' => 13,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 6126369,
+    'name' => 'LaserJet Pro P1102',
+    'kind' => 'принтер',
+    'description' => 'принтер, A4, печать  лазерная ч/б, 18 стр/мин ч/б, 2 Мб, USB',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '7240',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+      'discount' => '15',
+      'base' => '6497',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/6126369.jpeg',
+      'width' => 701,
+      'height' => 522,
+    ),
+    'link' => 'https://market.yandex.ru/product/6126369?hid=138608&pp=1001',
+    'vendorId' => 152722,
+    'vendor' => 'HP',
+    'isGroup' => false,
+    'reviewsCount' => 106,
+    'rating' => 4.0,
+    'offersCount' => 42,
+    'gradeCount' => 436,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 12659941,
+    'name' => 'L366',
+    'kind' => 'МФУ',
+    'description' => 'МФУ, A4, печать  пьезоэлектрическая струйная, 4-цветная, 33 стр/мин ч/б, 15 стр/мин цветн., USB, Wi-Fi, печать фото',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '14290',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/12659941.jpeg',
+      'width' => 701,
+      'height' => 292,
+    ),
+    'link' => 'https://market.yandex.ru/product/12659941?hid=138608&pp=1001',
+    'vendorId' => 153058,
+    'vendor' => 'Epson',
+    'isGroup' => false,
+    'reviewsCount' => 17,
+    'rating' => 3.5,
+    'offersCount' => 80,
+    'gradeCount' => 18,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 8463436,
+    'name' => 'PIXMA iP7240',
+    'kind' => 'принтер',
+    'description' => 'принтер, A4, печать  термическая струйная, 5-цветная, двусторонняя, 15 изобр./мин ч/б, 10 изобр./мин цветн., USB, Wi-Fi, печать фото, печать на CD/DVD',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '4990',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/8463436.jpeg',
+      'width' => 683,
+      'height' => 389,
+    ),
+    'link' => 'https://market.yandex.ru/product/8463436?hid=138608&pp=1001',
+    'vendorId' => 152861,
+    'vendor' => 'Canon',
+    'isGroup' => false,
+    'reviewsCount' => 90,
+    'rating' => 3.5,
+    'offersCount' => 68,
+    'gradeCount' => 149,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 13438777,
+    'name' => 'PIXMA G2400',
+    'kind' => 'МФУ',
+    'description' => 'МФУ, A4, печать  термическая струйная, 4-цветная, 8.8 изобр./мин ч/б, 5 изобр./мин цветн., USB, печать фото',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '12390',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/13438777.jpeg',
+      'width' => 556,
+      'height' => 260,
+    ),
+    'link' => 'https://market.yandex.ru/product/13438777?hid=138608&pp=1001',
+    'vendorId' => 152861,
+    'vendor' => 'Canon',
+    'isGroup' => false,
+    'reviewsCount' => 8,
+    'rating' => 4.5,
+    'offersCount' => 69,
+    'gradeCount' => 8,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 10677646,
+    'name' => 'Color LaserJet Pro MFP M176n (CF547A)',
+    'kind' => 'МФУ',
+    'description' => 'МФУ, A4, печать  лазерная, 4-цветная, 16 стр/мин ч/б, 4 стр/мин цветн., 128 Мб, Ethernet RJ-45, USB, ЖК-панель',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '19520',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10677646.jpeg',
+      'width' => 554,
+      'height' => 454,
+    ),
+    'link' => 'https://market.yandex.ru/product/10677646?hid=138608&pp=1001',
+    'vendorId' => 152722,
+    'vendor' => 'HP',
+    'isGroup' => false,
+    'reviewsCount' => 13,
+    'rating' => 3.5,
+    'offersCount' => 85,
+    'gradeCount' => 29,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 11030235,
+    'name' => 'LaserJet Pro M125ra',
+    'kind' => 'МФУ',
+    'description' => 'МФУ, A4, печать  лазерная ч/б, 20 стр/мин ч/б, 128 Мб, USB, ЖК-панель',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '11890',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/11030235.jpeg',
+      'width' => 531,
+      'height' => 424,
+    ),
+    'link' => 'https://market.yandex.ru/product/11030235?hid=138608&pp=1001',
+    'vendorId' => 152722,
+    'vendor' => 'HP',
+    'isGroup' => false,
+    'reviewsCount' => 34,
+    'rating' => 3.5,
+    'offersCount' => 41,
+    'gradeCount' => 57,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 10782150,
+    'name' => 'L120',
+    'kind' => 'принтер',
+    'description' => 'принтер, A4, печать  пьезоэлектрическая струйная, 4-цветная, 8.5 стр/мин ч/б, 4.5 стр/мин цветн., USB',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '7600',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10782150.jpeg',
+      'width' => 701,
+      'height' => 337,
+    ),
+    'link' => 'https://market.yandex.ru/product/10782150?hid=138608&pp=1001',
+    'vendorId' => 153058,
+    'vendor' => 'Epson',
+    'isGroup' => false,
+    'reviewsCount' => 18,
+    'rating' => 3.0,
+    'offersCount' => 74,
+    'gradeCount' => 31,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 6231083,
+    'name' => 'Stylus Photo P50',
+    'kind' => 'принтер',
+    'description' => 'принтер, A4, печать  пьезоэлектрическая струйная, 6-цветная, 37 стр/мин ч/б, 38 стр/мин цветн., USB, печать фото, печать на CD/DVD',
+    'categoryId' => 138608,
+    'category' => 
+    array (
+      'id' => 138608,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Принтеры и МФУ',
+    ),
+    'prices' => 
+    array (
+      'amount' => '15084',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/6231083.jpeg',
+      'width' => 595,
+      'height' => 477,
+    ),
+    'link' => 'https://market.yandex.ru/product/6231083?hid=138608&pp=1001',
+    'vendorId' => 153058,
+    'vendor' => 'Epson',
+    'isGroup' => false,
+    'reviewsCount' => 66,
+    'rating' => 4.5,
+    'offersCount' => 68,
+    'gradeCount' => 224,
+    'articlesCount' => 1,
+    'isNew' => 0,
+  ),
+)
